addea52f07e9d8150b11a50c9a6021814560e66f
= <milann17@live.com>
2016-11-15 10:14:47 +0100

    java controller change

--- src/main/java/com/maketest/client/IndexPageController.java
+++ src/main/java/com/maketest/client/IndexPageController.java
@@ -10,28 +10,27 @@ import javax.servlet.http.Cookie;
 /**
  * Created by Jovana Micic on 25-Oct-16.
  */
-@Controller
 public class IndexPageController {
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String indexPage(){
-        return "login";
-    }
-
-    @RequestMapping(value = "/registration", method = RequestMethod.GET)
-    public String registrationPage(){
-        return "registration";
-    }
-
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String index(){
-        return "index";
-    }
-
-    @RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String homePage(@RequestParam String mtt)
-    {
-        return "home";
-    }
+//    @RequestMapping(value = "/login", method = RequestMethod.GET)
+//    public String indexPage(){
+//        return "login";
+//    }
+//
+//    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+//    public String registrationPage(){
+//        return "registration";
+//    }
+//
+//    @RequestMapping(value = "/index", method = RequestMethod.GET)
+//    public String index(){
+//        return "index";
+//    }
+//
+//    @RequestMapping(value = "/home", method = RequestMethod.GET)
+//    public String homePage(@RequestParam String mtt)
+//    {
+//        return "home";
+//    }
 }
 
